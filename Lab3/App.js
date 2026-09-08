625f2b5272b735481728f49b14b613ed2dd51abf
Dipanshu Roy <roydipanshu02@gmail.com>
2026-09-08 10:13:23 +0530

    addhelper.js

--- Lab3/App.js
+++ Lab3/App.js
@@ -1,10 +1,34 @@
 import http from "http";
+//import * as teams from 'teams.js'
+import { getAllTeams } from  "./teams.js";
+
+const PORT = 5000
+
+const sendJson = (res , statusCode, data) => {
+    res.writeHead(statusCode,{"content-type":"application/json"});
+    res.end(data === undefined ? "" : JSON.stringify(data));
+};
+
+const parseJSONBody = (req) => {
+     return new Promise((resolve,reject) => {
+        let body = "";
+        req.on("data",(chunk)=>{
+            body += chunk.toString();
+        });
+        req.on("end" , () =>{
+            try {
+                resolve(body ? JSON.parse(body) : {});
+            } catch(error) {
+                reject(error);
+            }
+        });
+    });
+};
 
 const server = http.createServer((req,res) => {
 
-    res.end("<h1>Welcome to Server Side</h1>");
 });
 
 server.listen(5000, () => {
-    console.log("Server is running");
+    console.log("SIH Server is running at ",PORT);
 });
